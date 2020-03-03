bc5f3575409214afc2e94d2dc4476cf95cb4c14b
github username <github email address>
2020-03-03 09:06:15 -0600

    login

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@
 const container = document.querySelector('.card');
 const row = document.getElementById('first-row');
 let card
+let user = ""
+let username
+const userInput = document.getElementById('login-input');
+const loginButton = document.getElementById('login-submit');
+const loginForm = document.getElementById('login-form');
 
 const fetchProducts =() => {
     
@@ -25,15 +30,69 @@ const renderProducts = (product) => {
     })
 }
 
+// const userLogin
+// let user = ""
+// hit submit if value takes value 
+// do a fetch post to backend
+// findorcreateby on backend
+// render in json 
+// feed return value into new function 
+// set user = "name" 
+// set form to display=block (show) none (hide) 
 
 
+const fetchCurrentUser = () => {
+username = userInput.value
+event.preventDefault()
+if(username==""){
+    alert("Please enter username")
+} else {
+    fetch('http://localhost:3000/users')
+        .then( resp => resp.json() )
+        .then( currentUserData => renderCurrentUser(currentUserData) )
+}}
+    
 
 
+const renderCurrentUser = (currentUserData) => {
+    const userList = currentUserData.filter(data => data.username == userInput.value)
+    
+    if (userList[0]){
+        console.log("ture")
+        loginForm.innerHTML=`Welcome Back ${username}!`
+    } else {
+        
+        postNewUser()
+    }
+}
 
+const postNewUser = () => {
+    fetch('http://localhost:3000/users', createNewUser(username) )
+        .then( resp => resp.json() )
+        .then( newUserData => renderNewUser(newUserData) )
+}
+
+
+function createNewUser(username) {
+    return {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({
+        username: username
+      })
+    }
+  }
+
+const renderNewUser = (newUserData) => {
+    loginForm.innerHTML=`Welcome ${username}!`
+}
 
 
 //event listeners
-
+loginButton.addEventListener('click', fetchCurrentUser)
 
 //run it 
 fetchProducts()
