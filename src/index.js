e7b56605a7d2b24e4531d777862cbdb078f8053e
github username <github email address>
2020-03-03 15:25:54 -0600

    frontend

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 //vars 
-const container = document.querySelector('.card');
+const container = document.querySelector('.row');
 const row = document.getElementById('first-row');
 let card
 let user = ""
@@ -7,6 +7,9 @@ let username
 const userInput = document.getElementById('login-input');
 const loginButton = document.getElementById('login-submit');
 const loginForm = document.getElementById('login-form');
+const productDetailContainer = document.getElementById('product-detail')
+const cartDropdown = document.getElementById('cart-dropdown')
+const dropdownButton = document.getElementById('navbarDropdown')
 
 const fetchProducts =() => {
     
@@ -16,18 +19,59 @@ const fetchProducts =() => {
     
 }
 
-const renderProducts = (product) => {
-    product.forEach(product => {
-        // const card=`<div class="card"><h2>${product.name}</h2><img src="${product.image_url}"></div>`
-
-       const card=`<img src="${product.image_url}" class="card-img-top" alt="...">
-       <div class="card-body">
-         <h5 class="card-title">${product.name}</h5>
-         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+// const renderProducts = (product) => {
+//     product.forEach(product => {
+//        const card=`<div id="product-card" data-id=${product.id}><img data-id=${product.id} src="${product.image_url}" class="card-img-top" alt="...">
+//        <div class="card-body">
+//          <h5 class="card-title">${product.name}</h5>
+//          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
         
-       </div>`
-        container.innerHTML += card 
-    })
+//        </div></div>`
+//         container.innerHTML += card 
+//     })
+// }
+
+const renderProducts = product => {
+    product.forEach(product => {
+      // console.log(product);
+      const card = `
+      <div class="product-column col-lg-3 col-md-6">   
+      <div class="card">
+            <div class="card-header">
+              <h3>${product.name}</h3>
+            </div>
+            <div class="card-body">
+            <img class="card-img-top" src=${product.image_url}>
+              <h2 class="price-text">$${product.price}</h2>
+              <button class="see-more-button" data-id="${product.id} class="btn btn-lg btn-block btn-outline-dark" type="button">See More...</button>
+            </div>
+            </div>`;
+      container.innerHTML += card;
+    });
+  };
+
+const fetchOneProduct = () => {
+    const clicked = event.target
+    const productId = clicked.dataset.id
+    if(clicked.className=="see-more-button") {
+    fetch(`http://localhost:3000/products/${productId}`)
+        .then( resp => resp.json() )
+        .then( oneProductData => renderOneProduct(oneProductData) )
+    }
+}
+
+const renderOneProduct = (oneProductData) => {
+    const productInfo = oneProductData.data.attributes
+    const oneProductDetailCard = `<h1>${productInfo.name}</h1>
+    <img src=${productInfo.image_url} >
+    <h3>$${productInfo.price}</h3>
+    <p>${productInfo.description}</p>
+    <a class="nav-link" href="#product-detail">
+    <button id="add-to-cart" data-id=${productInfo.id} class="btn btn-info">
+      Add to Cart
+    </button></a>`
+    productDetailContainer.innerHTML = oneProductDetailCard
+    
 }
 
 // const userLogin
@@ -43,6 +87,7 @@ const renderProducts = (product) => {
 
 const fetchCurrentUser = () => {
 username = userInput.value
+
 event.preventDefault()
 if(username==""){
     alert("Please enter username")
@@ -55,11 +100,13 @@ if(username==""){
 
 
 const renderCurrentUser = (currentUserData) => {
+
     const userList = currentUserData.filter(data => data.username == userInput.value)
+    user = userList
     
     if (userList[0]){
-        console.log("ture")
         loginForm.innerHTML=`Welcome Back ${username}!`
+        
     } else {
         
         postNewUser()
@@ -90,9 +137,22 @@ const renderNewUser = (newUserData) => {
     loginForm.innerHTML=`Welcome ${username}!`
 }
 
+const fetchCarts = () => {
+    fetch('http://localhost:3000/carts')
+        .then(resp => resp.json() )
+        .then( cartData => renderCarts(cartData) )
+        
+}
+
+const renderCarts = (cartData) => {
+
+}
+
 
 //event listeners
 loginButton.addEventListener('click', fetchCurrentUser)
+container.addEventListener('click', fetchOneProduct)
+// dropdownButton.addEventListener('click', fetchCarts)
 
 //run it 
 fetchProducts()
