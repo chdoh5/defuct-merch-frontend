68b38e0fd187ae6b914fe33032f787ffce1bb434
maxmcfadden88 <maxmcfadden88@gmail.com>
2020-03-04 11:02:32 -0600

    maxfrontstuff

--- src/index.js
+++ src/index.js
@@ -1,25 +1,22 @@
-//vars 
-const container = document.querySelector('.row');
-const row = document.getElementById('first-row');
-let card
-let user = ""
-let username
-const userInput = document.getElementById('login-input');
-const loginButton = document.getElementById('login-submit');
-const loginForm = document.getElementById('login-form');
-const productDetailContainer = document.getElementById('product-detail')
-const cartDropdown = document.getElementById('cart-dropdown')
-const dropdownButton = document.getElementById('navbarDropdown')
-const cartItemList = document.getElementById('cart-item-list')
+//vars
+const container = document.querySelector(".row");
+const row = document.getElementById("first-row");
+let card;
+let user = "";
+let username;
+const userInput = document.getElementById("login-input");
+const loginButton = document.getElementById("login-submit");
+const loginForm = document.getElementById("login-form");
+const productDetailContainer = document.getElementById("product-detail");
+const cartDropdown = document.getElementById("cart-dropdown");
+const dropdownButton = document.getElementById("navbarDropdown");
+const cartItemList = document.getElementById("cart-item-list");
 
-
-const fetchProducts =() => {
-    
-    fetch('http://localhost:3000/products')
-    .then( resp => resp.json() )
-    .then( product => renderProducts(product) )
-    
-}
+const fetchProducts = () => {
+  fetch("http://localhost:3000/products")
+    .then(resp => resp.json())
+    .then(product => renderProducts(product));
+};
 
 // const renderProducts = (product) => {
 //     product.forEach(product => {
@@ -27,16 +24,16 @@ const fetchProducts =() => {
 //        <div class="card-body">
 //          <h5 class="card-title">${product.name}</h5>
 //          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        
+
 //        </div></div>`
-//         container.innerHTML += card 
+//         container.innerHTML += card
 //     })
 // }
 
 const renderProducts = product => {
-    product.forEach(product => {
-      // console.log(product);
-      const card = `
+  product.forEach(product => {
+    // console.log(product);
+    const card = `
       <div class="product-column col-lg-3 col-md-6">   
       <div class="card">
             <div class="card-header">
@@ -45,135 +42,130 @@ const renderProducts = product => {
             <div class="card-body">
             <img class="card-img-top" src=${product.image_url}>
               <h2 class="price-text">$${product.price}</h2>
-              <button class="see-more-button" data-id="${product.id} class="btn btn-lg btn-block btn-outline-dark" type="button">See More...</button>
+
+              <button  class="see-more-button" data-id="${product.id} class="btn btn-lg btn-block btn-outline-dark" type="button">
+               See More...
+              </button>
             </div>
             </div>`;
-      container.innerHTML += card;
-    });
-  };
+    container.innerHTML += card;
+  });
+};
 
 const fetchOneProduct = () => {
-    const clicked = event.target
-    const productId = clicked.dataset.id
-    if(clicked.className=="see-more-button") {
+  const clicked = event.target;
+  const productId = clicked.dataset.id;
+  if (clicked.className == "see-more-button") {
     fetch(`http://localhost:3000/products/${productId}`)
-        .then( resp => resp.json() )
-        .then( oneProductData => renderOneProduct(oneProductData) )
-    }
-}
+      .then(resp => resp.json())
+      .then(oneProductData => renderOneProduct(oneProductData));
+  }
+};
+// Change picture size in <img    >
+const renderOneProduct = oneProductData => {
+  const addToCartButton = document.getElementById("add-to-cart");
 
-const renderOneProduct = (oneProductData) => {
-    const addToCartButton = document.getElementById('add-to-cart')
-    const productInfo = oneProductData.data.attributes
-    const oneProductDetailCard = `<h1>${productInfo.name}</h1>
+  const productInfo = oneProductData.data.attributes;
+  const oneProductDetailCard = `<h1 id='foo'>${productInfo.name}</h1>
     <img src=${productInfo.image_url} >
     <h3>$${productInfo.price}</h3>
     <p>${productInfo.description}</p>
-    <a class="nav-link" href="#product-detail">
     <button id="add-to-cart" data-id=${oneProductData.data.id} class="btn btn-info">
       Add to Cart
-    </button></a>`
-    
-    
-    productDetailContainer.innerHTML = oneProductDetailCard
-    
-}
+    </button></a>`;
+
+  productDetailContainer.innerHTML = oneProductDetailCard;
+
+  window.location.href = "#foo";
+};
 
 // const userLogin
 // let user = ""
-// hit submit if value takes value 
+// hit submit if value takes value
 // do a fetch post to backend
 // findorcreateby on backend
-// render in json 
-// feed return value into new function 
-// set user = "name" 
-// set form to display=block (show) none (hide) 
-
+// render in json
+// feed return value into new function
+// set user = "name"
+// set form to display=block (show) none (hide)
 
 const fetchCurrentUser = () => {
-username = userInput.value
+  username = userInput.value;
 
-event.preventDefault()
-if(username==""){
-    alert("Please enter username")
-} else {
-    fetch('http://localhost:3000/users')
-        .then( resp => resp.json() )
-        .then( currentUserData => renderCurrentUser(currentUserData) )
-}}
-    
+  event.preventDefault();
+  if (username == "") {
+    alert("Please enter username");
+  } else {
+    fetch("http://localhost:3000/users")
+      .then(resp => resp.json())
+      .then(currentUserData => renderCurrentUser(currentUserData));
+  }
+};
 
+const renderCurrentUser = currentUserData => {
+  const userList = currentUserData.filter(
+    data => data.username == userInput.value
+  );
+  user = userList;
 
-const renderCurrentUser = (currentUserData) => {
-
-    const userList = currentUserData.filter(data => data.username == userInput.value)
-    user = userList
-    
-    if (userList[0]){
-        loginForm.innerHTML=`Welcome Back ${username}!`
-        
-    } else {
-        
-        postNewUser()
-    }
-}
+  if (userList[0]) {
+    loginForm.innerHTML = `Welcome Back ${username}!`;
+  } else {
+    postNewUser();
+  }
+};
 
 const postNewUser = () => {
-    fetch('http://localhost:3000/users', createNewUser(username) )
-        .then( resp => resp.json() )
-        .then( newUserData => renderNewUser(newUserData) )
-}
-
+  fetch("http://localhost:3000/users", createNewUser(username))
+    .then(resp => resp.json())
+    .then(newUserData => renderNewUser(newUserData));
+};
 
 function createNewUser(username) {
-    return {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json"
-      },
-      body: JSON.stringify({
-        username: username
-      })
-    }
-  }
-
-const renderNewUser = (newUserData) => {
-    loginForm.innerHTML=`Welcome ${username}!`
+  return {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify({
+      username: username
+    })
+  };
 }
+
+const renderNewUser = newUserData => {
+  loginForm.innerHTML = `Welcome ${username}!`;
+};
 
 const fetchCarts = () => {
-    cartItemList.innerHTML = []
-    fetch('http://localhost:3000/carts')
-        .then(resp => resp.json() )
-        .then( cartData => renderCarts(cartData) )
-        
-}
+  cartItemList.innerHTML = [];
+  fetch("http://localhost:3000/carts")
+    .then(resp => resp.json())
+    .then(cartData => renderCarts(cartData));
+};
 
-const renderCarts = (cartData) => {
-    const currentCart = cartData.filter(data => data.user_id === user[0].id)
-    currentCart.forEach(cart => {
-        const li = `<li>${cart.product.name}</li>`
-        
-        cartItemList.innerHTML += li
-        
-    })
+const renderCarts = cartData => {
+  const currentCart = cartData.filter(data => data.user_id === user[0].id);
+  currentCart.forEach(cart => {
+    const li = `<li>${cart.product.name}</li>`;
 
-}
+    cartItemList.innerHTML += li;
+  });
+};
 
 const postCart = () => {
-    event.preventDefault()
-    const clicked = event.target
-    const user_id = user[0].id
-    const product_id = clicked.dataset.id
-    
-    if(clicked.id==='add-to-cart') {
-        fetch('http://localhost:3000/carts', createNewCartObj(user_id, product_id) )
-            .then( resp => resp.json() )
-            .then( newCartData => console.log(newCartData) )
-    }
+  event.preventDefault();
+  const clicked = event.target;
+  const user_id = user[0].id;
+  const product_id = clicked.dataset.id;
 
-}
+  if (clicked.id === "add-to-cart") {
+    fetch("http://localhost:3000/carts", createNewCartObj(user_id, product_id))
+      .then(resp => resp.json())
+      .then(newCartData => console.log(newCartData));
+  }
+};
 
 // const createNewCartObj = (user_id, product_id) => {
 //     return {
@@ -190,30 +182,26 @@ const postCart = () => {
 // }
 
 function createNewCartObj(user_id, product_id) {
-    return {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json"
-      },
-      body: JSON.stringify({
-        user_id: user_id,
-        product_id: product_id
-      })
-    }
-  }
-
-const renderNewCart = (newCartData) => {
-
+  return {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify({
+      user_id: user_id,
+      product_id: product_id
+    })
+  };
 }
 
-
+const renderNewCart = newCartData => {};
 
 //event listeners
-loginButton.addEventListener('click', fetchCurrentUser)
-container.addEventListener('click', fetchOneProduct)
-dropdownButton.addEventListener('click', fetchCarts)
-productDetailContainer.addEventListener('click', postCart )
+loginButton.addEventListener("click", fetchCurrentUser);
+container.addEventListener("click", fetchOneProduct);
+dropdownButton.addEventListener("click", fetchCarts);
+productDetailContainer.addEventListener("click", postCart);
 
-//run it 
-fetchProducts()
+//run it
+fetchProducts();
